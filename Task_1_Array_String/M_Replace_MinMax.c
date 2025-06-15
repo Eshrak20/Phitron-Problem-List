e24d448aa0bc667db4c4dd9734dd1a0bb2c45204
Eshrak20 <uemran2018@gmail.com>
2025-06-15 07:10:36 +0600

    First problem done

--- Task_1_Array_String/M_Replace_MinMax.c
+++ Task_1_Array_String/M_Replace_MinMax.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
+#include <limits.h>
 int main()
 {
     int x;
     scanf("%d", &x);
     int arr[x];
+    int max = INT_MIN;
+    int maxPos = 0;
+    int min = INT_MAX;
+    int minPos = 0;
+    for (int i = 0; i < x; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < x; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            maxPos = i;
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            minPos = i;
+        }
+    }
+    arr[maxPos] = min;
+    arr[minPos] = max;
 
     for (int i = 0; i < x; i++)
     {
-        scanf("%d", &arr[x]);
-        // printf("%d ", arr[x]);
+        printf("%d ", arr[i]);
     }
-  
-    printf("%d ", arr[1]);
 
     return 0;
 }
