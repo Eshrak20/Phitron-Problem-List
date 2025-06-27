5bb8977a1d8ae27c17163cfc44009b8299c442cf
Eshrak20 <uemran2018@gmail.com>
2025-06-27 07:22:18 +0600

    G done

--- Task_1_Array_String/G_Conversion.c
+++ Task_1_Array_String/G_Conversion.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-    char Eshrak[1000];
-    fgets(Eshrak, sizeof(Eshrak), stdin);
-    printf("%s",Eshrak);
-    
-     // char fardin[100];
-    // scanf("%s",&fardin);
-    // printf("%s",fardin);
+    char a[100001];
+    // fgets(a, 1000, stdin);
+    scanf("%s", a);
+    for (int i = 0; i < strlen(a); i++)
+    {
+        if (a[i] == 44)
+        {
+            a[i] = 32;
+        }
+        else if (a[i] >= 65 && a[i] < 97)
+        {
+            a[i] = a[i] + 32;
+        }
+        else
+        {
+            a[i] = a[i] - 32;
+        }
+    }
+    printf("%s", a);
 
     return 0;
 }
